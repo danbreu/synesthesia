6c1de29514222b94a816b58bf5e1a49efc6335b6
Dominik Klisa <dominik.klisa@outlook.com>
2022-11-15 23:57:16 +0100

    init express

--- README.md
+++
@@ -1,5 +0,0 @@
-# Synesthesia
-## Build JavaScript File
-1. Navigate to bin.
-2. Install requirements `npm ci`
-3. Bundle code with `npm run build`
--- server.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-from flask_cors import CORS
-
-app = Flask(__name__, static_url_path='/', template_folder='static')
-CORS(app)
-
-@app.route("/")
-def index():
-    return render_template("index.html")
