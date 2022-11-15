2f7bba97ff690b384c33d7d96f2569e60f8f6b1b
Dominik Klisa <dominik.klisa@outlook.com>
2022-11-16 00:30:23 +0100

    add linter

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
 
-var server = express();
+const server = express()
 
-server.use(logger('dev'));
-server.use(express.json());
-server.use(express.urlencoded({ extended: false }));
-server.use(cookieParser());
-server.use('/', express.static(path.join(__dirname, 'public')));
+server.use(logger('dev'))
+server.use(express.json())
+server.use(express.urlencoded({ extended: false }))
+server.use(cookieParser())
+server.use('/', express.static(path.join(__dirname, 'public')))
 
-module.exports = server;
+module.exports = server
