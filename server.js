d4d0475b94f1e8efb302da1ffa157cbde014482c
Dominik Klisa <dominik.klisa@outlook.com>
2022-11-16 03:06:33 +0100

    add compression to express server

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const express = require('express')
 const path = require('path')
 const logger = require('morgan')
+const compression = require('compression')
 
 const server = express()
 
+server.use(compression({ filter: shouldCompress }))
 server.use(logger('dev'))
 server.use('/', express.static(path.join(__dirname, 'public')))
+
+function shouldCompress (req, res) {
+	if (req.headers['x-no-compression']) {
+		// don't compress responses with this request header
+		return false
+	}
+
+	// fallback to standard filter function
+	return compression.filter(req, res)
+}
 
 module.exports = server
