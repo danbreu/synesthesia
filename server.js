2138f0106e74b9551e8e759c0eaa6411fcd8feeb
Dominik Klisa <dominik.klisa@outlook.com>
2022-11-16 01:22:56 +0100

    remove unused modules

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 const express = require('express')
 const path = require('path')
-const cookieParser = require('cookie-parser')
 const logger = require('morgan')
 
 const server = express()
 
 server.use(logger('dev'))
-server.use(express.json())
-server.use(express.urlencoded({ extended: false }))
-server.use(cookieParser())
 server.use('/', express.static(path.join(__dirname, 'public')))
 
 module.exports = server
